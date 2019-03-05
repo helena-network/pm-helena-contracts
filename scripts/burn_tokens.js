e20b344d1bddcf0d597ac7c751b8f4e80331c32e
Carlos Buendía <cbuendia@influity.com>
2019-03-05 12:21:07 +0100

    Feat: Includes xDai

---
+++ scripts/burn_tokens.js
@@ -0,0 +1,13 @@
+module.exports = require('./play_token_cli_wrapper')(
+  artifacts,
+  async function(PlayToken, args) {
+    if(!args.amount || !args.to) {
+      throw "[Error] need to set --amount and --to parameters"
+    }
+    const instance = await PlayToken.deployed()
+    await instance.burn(args.to, args.amount)
+    console.log(`${args.amount} tokens issued to the following accounts:\n${
+      args.to.replace(',', '\n')
+    }`)
+  }
+)
