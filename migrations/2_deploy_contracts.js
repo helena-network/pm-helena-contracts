eebbe4cf00c67af7a2d87fff832ea0cbf9d83cfc
Carlos Buendía <cbuendia@influity.com>
2019-03-25 00:50:33 +0100

    build(migrations): fixes playtoken deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 const Proton = artifacts.require('PlayToken')
-//const args = require('yargs').argv;
 
 module.exports = function(deployer) {
-    deployer.deploy(Proton, "asdf", 'xProton', 'xP+',  20, operators, 0, 1000);
+    deployer.deploy(Proton, 'xProton', 'xP+',  20, [], 0);
 }
