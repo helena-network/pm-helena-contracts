ad59b8500951d4c7476b253727e69a689b64daf4
Carlos Buendía <cbuendia@influity.com>
2019-03-24 23:21:45 +0100

    build: Included pre-test script to deploy ERC820

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-const Proton = artifacts.require('ReferenceToken')
+const Proton = artifacts.require('PlayToken')
 //const args = require('yargs').argv;
 
 module.exports = function(deployer) {
-    deployer.deploy(Proton, "asdf", "asdf", 20, ['0x08c9906A56b7D6EC75676Db0E307921ED7438989'], '0x08c9906A56b7D6EC75676Db0E307921ED7438989', 1000);
+    deployer.deploy(Proton, "asdf", 'xProton', 'xP+',  20, operators, 0, 1000);
 }
